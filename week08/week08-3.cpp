30a76aad262b6ca456c7425a6cbc062f0ee4aa8b
r1i2c3h4 <sssssss0526@gmail.com>
2025-05-02 11:40:14 +0800

    week08

---
+++ week08/week08-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSymmetricIntegers(int low, int high) {
+        int ans = 0; // 迴圈前面 ans 是 0
+        for(int i=low; i<=high; i++){
+            int a = i/1000;      // 千位數 (實習課 第6週 基礎題)
+            int b = i/100 % 10;  // 百位數
+            int c = i/10 % 10;   // 十位數
+            int d = i % 10;      // 個位數
+            if( 11<=i && i<=99 && c==d) ans++;  // 2位數
+            if(1000<=i && i<=9999 && a+b==c+d) ans++; // 4 位數
+        } // 迴圈中間 ans++
+        return ans; // 迴圈後面, 把 ans 拿來用
+    }
+};
